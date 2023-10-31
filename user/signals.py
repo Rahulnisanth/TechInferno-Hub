3c8196dbe9dfa04a59e5b9a9b44bfd45d4497808
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2023-10-31 14:16:04 +0530

    reset-passkey assisted

--- user/signals.py
+++ user/signals.py
@@ -2,6 +2,8 @@ from django.dispatch import receiver
 from django.db.models.signals import post_delete, post_save
 from django.contrib.auth.models import User 
 from .models import *
+from django.core.mail import send_mail
+from django.conf import settings
 
 @receiver(post_save, sender=User)
 def createProfile(sender, instance, created, **kwargs):
@@ -12,6 +14,17 @@ def createProfile(sender, instance, created, **kwargs):
                   username=user.username,
                   email=user.email,
             )
+            # Email config ->
+            subject = f'ProjectCaster welcomes you Mr/Mrs {profile.username}!'
+            message = "Lorem ipsum is a placeholder text commonly used in the publishing and typography industries to demonstrate the layout and formatting of a document without revealing any actual content. Lorem ipsum is a placeholder text commonly used in the publishing and typography industries to demonstrate the layout and formatting of a document without revealing any actual content."
+            send_mail(
+                  subject,
+                  message,
+                  settings.EMAIL_HOST_USER,
+                  [profile.email],
+                  fail_silently=False
+            )
+
 
 @receiver(post_save, sender=Profile)
 def updateProfile(sender, instance, created, **kwargs):
