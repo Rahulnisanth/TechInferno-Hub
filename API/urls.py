59dfcfe4f300f9090e88e6d3056e433ce7e950a9
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2023-11-15 21:54:50 +0530

    unknown-commit

--- API/urls.py
+++ API/urls.py
@@ -2,20 +2,17 @@ from django.urls import path
 from . import views
 
 from rest_framework_simplejwt.views import (
-      TokenObtainPairView,
-      TokenRefreshView,
+    TokenObtainPairView,
+    TokenRefreshView,
 )
 
 urlpatterns = [
-
-      path('profiles/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-      path('profiles/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-      path('', views.getRoutes),
-      path('projects/', views.getProjects),
-      path('project/<str:pk>/', views.getProject),
-      path('profiles/', views.getProfiles),
-      path('profile/<str:pk>/', views.getProfile),
-      path('remove-tag/', views.removeTag),
-
+    path("profiles/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("profiles/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("", views.getRoutes),
+    path("projects/", views.getProjects),
+    path("project/<str:pk>/", views.getProject),
+    path("profiles/", views.getProfiles),
+    path("profile/<str:pk>/", views.getProfile),
+    path("remove-tag/", views.removeTag),
 ]
